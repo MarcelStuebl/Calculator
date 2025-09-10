24af2db7a2ac4503d118cca43ec6f5048939dfd1
Marcel <123411996+MarcelStuebl@users.noreply.github.com>
2025-09-10 10:04:45 +0200

    Push

--- src/main/java/htl/steyr/calculator/CalculatorController.java
+++ src/main/java/htl/steyr/calculator/CalculatorController.java
@@ -3,7 +3,6 @@ package htl.steyr.calculator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
